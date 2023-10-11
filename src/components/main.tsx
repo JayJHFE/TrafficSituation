fa2e9843c73300614f3acc34d007de44beb41e67
jhlee <dove92lee@gmail.com>
2023-10-11 17:29:51 +0900

    티맵 맵띄우기 성공

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
 
+declare global {
+    interface Window {
+        Tmapv2: any;
+    }
+}
 export default function Main() {
     const {Tmapv2} = window;
     function initTmap(){
-        const map = new window.Tmapv2.Map("map_div",
-        {
-            center: new Tmapv2.LatLng(37.566481622437934,126.98502302169841), // 지도 초기 좌표
-            width: "890px",
-            height: "400px",
-            zoom: 15
-        });
+        const mapDiv = document.getElementById('map_div');
+        if (!mapDiv.firstChild) {
+            const map = new Tmapv2.Map("map_div", {
+              center: new window.Tmapv2.LatLng(37.566481622437934,126.98502302169841),
+              width: "890px",
+              height: "400px",
+              zoom: 15
+            });
+          }
     }
     useEffect(() => {
         // 컴포넌트가 렌더링된 후에 실행될 코드
