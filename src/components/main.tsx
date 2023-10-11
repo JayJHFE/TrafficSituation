f855d774db17fdc7775d75e0be3f25b00a03cd09
jhlee <dove92lee@gmail.com>
2023-10-11 17:39:34 +0900

    티맵 백터맵으로 변경

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,21 +2,22 @@ import React, { useEffect } from 'react';
 
 declare global {
     interface Window {
-        Tmapv2: any;
+        Tmapv3: any;
     }
 }
 export default function Main() {
-    const {Tmapv2} = window;
+    const {Tmapv3} = window;
     function initTmap(){
         const mapDiv = document.getElementById('map_div');
         if (!mapDiv.firstChild) {
-            const map = new Tmapv2.Map("map_div", {
-              center: new window.Tmapv2.LatLng(37.566481622437934,126.98502302169841),
-              width: "890px",
-              height: "400px",
-              zoom: 15
-            });
-          }
+        const map = new Tmapv3.Map("map_div",
+        {
+            center: new Tmapv3.LatLng(37.566481622437934,126.98502302169841), // 지도 초기 좌표
+            width: "890px",
+            height: "400px",
+            zoom: 18
+        });
+    }
     }
     useEffect(() => {
         // 컴포넌트가 렌더링된 후에 실행될 코드
